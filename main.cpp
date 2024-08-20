d453a9550c6738734ba4d21bcf1f00d4718d22fe
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-21 02:40:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+   int a ;
+   cin >> a  ;
+
+   for(int i =1;i<=a;i++)
+   {
+       cout << i << endl;
+   }
+
+
+    return 0;
+}
